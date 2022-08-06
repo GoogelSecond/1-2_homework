bbb8f26d4b16778b9f839a1642cf7c2fec77dead
Nikita Gugel <googelfist@gmail.com>
2022-08-06 12:48:53 +0500

    add zero button, add logic, change app name

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,20 +3,24 @@ package com.example.helloworld
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0
     private lateinit var textView: TextView
+    private lateinit var zeroButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(LOG_TAG, "Hello World");
         textView = findViewById(R.id.show_count)
+        zeroButton = findViewById(R.id.button_zero)
     }
 
     companion object {
@@ -32,10 +36,27 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun countUp(view: View) {
+    fun countUp(button: View) {
         mCount++
+
+        if (mCount % 2 ==0) {
+            val evenColor = ContextCompat.getColor(this, android.R.color.holo_green_dark)
+            button.setBackgroundColor(evenColor)
+        } else {
+            val oddColor = ContextCompat.getColor(this, android.R.color.holo_blue_dark)
+            button.setBackgroundColor(oddColor)
+        }
         if (mCount != 0) {
             textView.text = mCount.toString()
+            val changedZeroColor = ContextCompat.getColor(this, android.R.color.holo_purple)
+            zeroButton.setBackgroundColor(changedZeroColor)
         }
+    }
+
+    fun setZeroCount(button: View) {
+        val greyColor = ContextCompat.getColor(this, android.R.color.darker_gray)
+        button.setBackgroundColor(greyColor)
+        mCount = 0
+        textView.text = mCount.toString()
     }
 }
