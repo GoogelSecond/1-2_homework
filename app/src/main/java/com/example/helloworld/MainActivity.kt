cc4d48dc605e9e5f2bbd4e82e9c688dfabd0b990
Nikita Gugel <googelfist@gmail.com>
2022-08-06 11:07:20 +0500

    comlete 1_2 lesson

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,16 +2,40 @@ package com.example.helloworld
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
+    private var mCount = 0
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(LOG_TAG, "Hello World");
+        textView = findViewById(R.id.show_count)
     }
 
     companion object {
         private val LOG_TAG = MainActivity::class.java.simpleName
+    }
+
+    fun showToast(view: View) {
+        val toast: Toast = Toast.makeText(
+            this, R.string.toast_message,
+            Toast.LENGTH_SHORT
+        )
+        toast.show()
+
+    }
+
+    fun countUp(view: View) {
+        mCount++
+        if (mCount != 0) {
+            textView.text = mCount.toString()
+        }
     }
 }
