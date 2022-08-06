b4b34f98cae56e8a75382ac13400dbbb557570b5
Nikita Gugel <googelfist@gmail.com>
2022-08-06 10:40:31 +0500

    add logging to main activity

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(LOG_TAG, "Hello World");
+    }
+
+    companion object {
+        private val LOG_TAG = MainActivity::class.java.simpleName
     }
 }
