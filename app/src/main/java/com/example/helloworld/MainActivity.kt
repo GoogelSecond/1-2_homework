3cc1e8140ad6c6424ee5f527194684d859c52f6d
Nikita Gugel <googelfist@gmail.com>
2022-08-07 13:04:44 +0500

    complete 1_3 lesson

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -11,52 +11,9 @@ import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
-    private var mCount = 0
-    private lateinit var textView: TextView
-    private lateinit var zeroButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(LOG_TAG, "Hello World");
-        textView = findViewById(R.id.show_count)
-        zeroButton = findViewById(R.id.button_zero)
-    }
-
-    companion object {
-        private val LOG_TAG = MainActivity::class.java.simpleName
-    }
-
-    fun showToast(view: View) {
-        val toast: Toast = Toast.makeText(
-            this, R.string.toast_message,
-            Toast.LENGTH_SHORT
-        )
-        toast.show()
-
-    }
-
-    fun countUp(button: View) {
-        mCount++
-
-        if (mCount % 2 ==0) {
-            val evenColor = ContextCompat.getColor(this, android.R.color.holo_green_dark)
-            button.setBackgroundColor(evenColor)
-        } else {
-            val oddColor = ContextCompat.getColor(this, android.R.color.holo_blue_dark)
-            button.setBackgroundColor(oddColor)
-        }
-        if (mCount != 0) {
-            textView.text = mCount.toString()
-            val changedZeroColor = ContextCompat.getColor(this, android.R.color.holo_purple)
-            zeroButton.setBackgroundColor(changedZeroColor)
-        }
-    }
-
-    fun setZeroCount(button: View) {
-        val greyColor = ContextCompat.getColor(this, android.R.color.darker_gray)
-        button.setBackgroundColor(greyColor)
-        mCount = 0
-        textView.text = mCount.toString()
     }
 }
