86d57440bde93ea688b238aa69d44005ea4d7b2c
Nikita Gugel <googelsecond@gmail.com>
2022-08-08 16:14:19 +0500

    add second activity

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.helloworld
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,5 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity(view: View) {
+        Log.d(LOG_TAG, "Button clicked!")
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        private val LOG_TAG = MainActivity::class.java.simpleName
     }
 }
