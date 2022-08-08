ace4f929caa07d176a11e2ee21d09d3e55328a97
Nikita Gugel <googelsecond@gmail.com>
2022-08-08 16:21:41 +0500

    add send data logic between activities

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,23 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var messageEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        messageEditText = findViewById(R.id.editText_main)
     }
 
     fun launchSecondActivity(view: View) {
         Log.d(LOG_TAG, "Button clicked!")
+        val message = messageEditText.text.toString()
         val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra(EXTRA_MESSAGE, message)
         startActivity(intent)
     }
 
     companion object {
         private val LOG_TAG = MainActivity::class.java.simpleName
+
+        const val EXTRA_MESSAGE = "com.example.helloworld.extra.MESSAGE"
+
     }
 }
--- app/src/main/java/com/example/helloworld/SecondActivity.kt
+++ app/src/main/java/com/example/helloworld/SecondActivity.kt
@@ -1,11 +1,15 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+        val textView = findViewById<TextView>(R.id.text_message)
+        textView.text = message
     }
 }
